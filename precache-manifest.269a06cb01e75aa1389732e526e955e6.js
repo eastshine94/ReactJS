32bfedd8dfe980e271333bbc6cd500cc85b26b0c
KimHD <eastshine94@naver.com>
2019-06-27 12:15:01 +0900

    Updates

---
+++ precache-manifest.269a06cb01e75aa1389732e526e955e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca5ff270366c464fc1ffc9467ba4c81",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0989eb445ccb1d147737",
+    "url": "/movie_app/static/css/main.4a964583.chunk.css"
+  },
+  {
+    "revision": "fffe6565ecdc5d244d6e",
+    "url": "/movie_app/static/js/2.248c7fd0.chunk.js"
+  },
+  {
+    "revision": "0989eb445ccb1d147737",
+    "url": "/movie_app/static/js/main.936f80c8.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
